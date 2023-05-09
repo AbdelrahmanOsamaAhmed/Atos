934d58292305db9a2fd1c4896288fcc81b3ca969
Abdelrahman Osama <69199570+AbdelrahmanOsamaAhmed@users.noreply.github.com>
2023-05-09 08:16:02 +0300

    Revert "MicroServices app"
    This reverts commit aff13d13af9120d6e80f2807f40849b257a65fb8.

--- Concepts Applications/MicroServices/backend/shoppingCart/models/cartItem.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const cartItemSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  price: { type: Number, required: true },
-});
-module.exports = mongoose.model("CartItem", cartItemSchema);
