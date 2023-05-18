9ebdf79986dd8f16fa966c974c7ef7c6fe679e46
Abdelrahman Osama <69199570+AbdelrahmanOsamaAhmed@users.noreply.github.com>
2023-05-18 13:21:10 +0300

    Final color accent + some styling tweaks

--- Tasks and Projects/Project Task 1/frontend/src/components/UI/NavBar/NavBar.js
+++ Tasks and Projects/Project Task 1/frontend/src/components/UI/NavBar/NavBar.js
@@ -1,70 +1,80 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../../contexts/auth-context";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Navbar } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
-import './NavBar.css'
+import "./NavBar.css";
 
 const NavBar = () => {
   const navigate = useNavigate();
   const { isLoggedIn, logout, userType } = useContext(AuthContext);
   return (
-    <div className="w100 fixed-top" style={{ backgroundColor: "transparent" }}>
-      <Container className="d-flex justify-content-between p-3">
-        
-          <Link className="navbar__brand" style={{ marginTop: "10px" }} to={"/"}>
-            Home
-          </Link>
-        {!isLoggedIn && (
-          <div className="d-flex" style={{ gap: "5px" }}>
-            <button
-              className="navbar__btn"
-              onClick={() => navigate("/signup")}
-              variant="outline-primary"
-            >
-              Sign up
-            </button>
-            <button
-              className="navbar__btn" onClick={() => navigate("/login")}>Log in</button>
-          </div>
-        )}
-        {isLoggedIn && (
-          <div className="d-flex" style={{ gap: "10px" }}>
-            {userType === "SUPER_ADMIN" && (
+    <Navbar
+      className="w100 fixed-top "
+      style={{ backgroundColor: "transparent" }}
+      expand="lg"
+    >
+      <Container>
+        <Link className="navbar__brand" to={"/"}>
+          Home
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end">
+          {!isLoggedIn && (
+            <div className="d-flex justify-content-end" style={{ gap: "10px" }}>
               <button
                 className="navbar__btn"
-                onClick={() => navigate("/create-admin")}
+                onClick={() => navigate("/signup")}
+                variant="outline-primary"
               >
-                Create Admin
+                Sign up
               </button>
-            )}
-            {userType === "TEACHER" && (
               <button
                 className="navbar__btn"
-                onClick={() => navigate("/create-question")}
+                onClick={() => navigate("/login")}
               >
-                Create Question
+                Log in
               </button>
-            )}
-            <button
-              className="navbar__btn"
-              onClick={() => navigate("/profile")}
-            >
-              Profile
-            </button>
-            <button
-              className="navbar__btn"
-              onClick={() => {
-                logout();
-                navigate("/login");
-              }}
-              variant="outline-primary"
-            >
-              Logout
-            </button>
-          </div>
-        )}
+            </div>
+          )}
+          {isLoggedIn && (
+            <div className="d-flex navbar__btn-wrapper" style={{ gap: "10px" }}>
+              {userType === "SUPER_ADMIN" && (
+                <button
+                  className="navbar__btn"
+                  onClick={() => navigate("/create-admin")}
+                >
+                  Create Admin
+                </button>
+              )}
+              {userType === "TEACHER" && (
+                <button
+                  className="navbar__btn"
+                  onClick={() => navigate("/create-question")}
+                >
+                  Create Question
+                </button>
+              )}
+              <button
+                className="navbar__btn"
+                onClick={() => navigate("/profile")}
+              >
+                Profile
+              </button>
+              <button
+                className="navbar__btn"
+                onClick={() => {
+                  logout();
+                  navigate("/login");
+                }}
+                variant="outline-primary"
+              >
+                Logout
+              </button>
+            </div>
+          )}
+        </Navbar.Collapse>
       </Container>
-    </div>
+    </Navbar>
   );
 };
 
